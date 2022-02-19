349e98224100e6f925ca36331946389127b6200f
Diego A. Dias <dtdias@icatuseguros.com.br>
2022-02-19 12:55:42 -0300

    .

--- Introdução C#/2022.5/Solucao_etapa3/Solucao_etapa3/BusinessException.cs
+++ Introdução C#/2022.5/Solucao_etapa3/Solucao_etapa3/BusinessException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Solucao_etapa3
 {
-    class BusinessException : Exception
+    public class BusinessException : Exception
     {
         public BusinessException(string Mensagem)
                  : base(Mensagem)
