c8b9b0475dd629dd2e8146281f43bb20ccc510ef
Diego A. Dias <dtdias@icatuseguros.com.br>
2022-03-04 14:19:43 -0300

    .

---
+++ Introdução C#/2022.8/SolucaoEtapa5/ClassLibraryEtapa5/Classes.cs
@@ -0,0 +1,61 @@
+﻿namespace ClassLibraryEtapa5
+{
+    public abstract class Pessoa
+    {
+         string _nome;
+         string _cpf;
+         
+        
+        public Pessoa( string nome, string cpf) 
+        {
+            _nome = nome;
+            _cpf = cpf;
+        }
+
+        public virtual void Escrever()
+        {
+            Console.WriteLine("Eu sou uma Pessoa");
+        }
+    }
+
+    public class Colaborador : Pessoa
+    {
+        public int _matricula;
+        private readonly bool _colaboradorEspecial;
+        public static int bonus;
+        public float _salario;
+       
+
+        public Colaborador(int matricula, string nome, string cpf, bool especial, float salario):base(nome,cpf)
+        {
+            _matricula = matricula;            
+            _colaboradorEspecial = especial;
+            _salario = salario;
+        }
+
+        public float calculaPLR()
+        {
+            return _salario * bonus ;
+        }       
+       
+        public override void Escrever()
+        {
+            Console.WriteLine("EU SOU UM COLABORADOR !");
+        }        
+       
+    }
+
+    public class Cliente : Pessoa
+    {
+        decimal _valorCompra;
+        public Cliente(string nome, string cpf, decimal valorCompra):base(nome,cpf)
+        {            
+            _valorCompra = valorCompra;
+        }
+
+        public override void Escrever()
+        {
+            Console.WriteLine("EU SOU UM CLIENTE");
+        }
+    }
+}
---
+++ Introdução C#/2022.8/SolucaoEtapa5/Etapa5_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using  ClassLibraryEtapa5;
+class Programa
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Colaborador.bonus = 2;
+
+            var colaborador1 = new Colaborador(111, "Carlo", "08755598834",true,10000);
+            var colaborador2 = new Colaborador(222, "Teles", "08755598834", true, 10000);
+            var colaborador3 = new Colaborador(333, "Lucas", "08755598834", true, 10000);
+            var colaborador4 = new Colaborador(444, "Rodrigo", "08755598834", true, 10000);
+
+            colaborador1.calculaPLR();
+            colaborador2.calculaPLR();
+            colaborador3.calculaPLR();
+            colaborador4.calculaPLR();
+
+            //Alterando o Bonus
+            Colaborador.bonus = 10;
+            
+            colaborador1.calculaPLR();
+            colaborador2.calculaPLR();
+            colaborador3.calculaPLR();
+            colaborador4.calculaPLR();
+
+            Pessoa p1 = colaborador1;
+            
+
+            colaborador1.Escrever();
+            p1.Escrever();
+            
+            
+            Cliente cliente1 = new Cliente("Teles", "08722533347", 10000);
+            Pessoa p2 = cliente1;
+
+            cliente1.Escrever();
+            p2.Escrever(); 
+
+        }
+        catch (InfnetNegocioException e)
+        {
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+}
