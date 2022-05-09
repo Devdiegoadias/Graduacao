879505319174e43335f90ccf5ba13073715a2ca6
Diego A. Dias <dtdias@icatuseguros.com.br>
2022-05-09 09:45:21 -0300

    aula

---
+++ Azure/0505AulaAzure/0505AulaAzure/Fila.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+namespace _0505AulaAzure
+{
+    public class Fila
+    {
+        public void CreateQueueClient(string queueName)
+        {
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+        }
+
+        public bool CreateQueue(string queueName)
+        {
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            queueClient.CreateIfNotExists();
+
+            if (queueClient.Exists())
+            {
+                Console.WriteLine("Fila Criada:" + queueName);
+            }
+
+            return true;
+        }
+
+        public void InsertMessage(string queueName, string message)
+        {
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            queueClient.CreateIfNotExists();
+
+            if (queueClient.Exists())
+                queueClient.SendMessage(message);
+
+            Console.WriteLine("Mensagem Inserida: " + message);
+        }
+
+        public void DequeueMessage(string queueName)
+        {
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            if (queueClient.Exists())
+            {
+                QueueMessage[] retrievedMessage = queueClient.ReceiveMessages();
+
+                Console.WriteLine("Retirado da Fila:" + retrievedMessage[0].Body);
+
+                queueClient.DeleteMessage(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+
+            }
+        }
+
+        public void UpdateMessage(string queueName)
+        {
+            string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+            QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+            if (queueClient.Exists())
+            {
+                QueueMessage[] message = queueClient.ReceiveMessages();
+
+                queueClient.UpdateMessage(message[0].MessageId, message[0].PopReceipt, "Atualizando2", TimeSpan.FromSeconds(5.0));
+            }
+        }
+
+    }
+}
---
+++ Azure/0505AulaAzure/0505AulaAzure/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Storage.Queues;
+using System.Configuration;
+using Azure.Storage.Queues.Models;
+
+namespace _0505AulaAzure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fila obj = new Fila();            
+
+            obj.InsertMessage("fila2", "1111");
+            obj.InsertMessage("fila2", "2222");
+            obj.InsertMessage("fila2", "3333");
+            obj.InsertMessage("fila2", "4444");
+            obj.InsertMessage("fila2", "5555");
+
+            obj.DequeueMessage("fila2");
+
+            obj.UpdateMessage("fila2");
+
+            Console.ReadKey();
+        }
+
+    }
+}
